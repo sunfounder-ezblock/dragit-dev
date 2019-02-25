f33e509cabadc59b4756dbf2fa701080b87533ab
Devin <1262088901@qq.com>
2019-02-25 15:11:08 +0800

    daisy add tutorial

--- packages/raspberrypi/tutorials/block.js
+++ packages/raspberrypi/tutorials/block.js
@@ -116,3 +116,28 @@ RaspberryPi.tutorials.blockcode['Advance']['Wiring'] = {
         "image": "",
     },
 }
+RaspberryPi.tutorials.blockly['Getting Start']['Hello World'] = {
+    "name": 'Hello World', // Name: Blink LED
+    "steps": { // All Steps
+        "Introduction": { // Step 1
+            "description": "First project using the Raspberry Pi and the EZblock App. This project is used to learn more about Debug Monitor, it is a very useful tool to finish your projects.", // Step 1 description
+            "images": "", // Step 1 image
+        },
+        "Debug Monitor": { // Step 2
+            "description": "After we create a new project, we can see a debug monitor in the left functional area. The debug monitor is used to print string, values or debug your code.", // Step 2 description
+            "images": "tutorial-basic-hello-world-debug-monitor", // Step 2 image
+        },
+        "Print String": { // Step 2
+            "description": "Drag out 2 print block from the Basic category and drop to forever block, then input “What is your dream” and “My dream is world peace”. After flash the code to the Raspberry Pi, you can see the 2 string appear on the debug monitor.", // Step 2 description
+            "images": "tutorial-basic-hello-world-print-string", // Step 2 image
+        },
+        "Print Value": { // Step 2
+            "description": "It is a great tool if you want to read the value of one pins in your Raspberry Pi. Place a print block from the Basic category into forever block. Add a get pin A0 value block after the print block to print the A0 value. After flash the code to the Raspberry Pi, you can see the analog value of A0.", // Step 2 description
+            "images": "tutorial-basic-hello-world-print-value", // Step 2 image
+        },
+        "Debug Code": { // Step 2
+            "description": "DO NOT ignore any errors appeared on the debug monitor, if you can’t solve the error, then the code will never download successfully.", // Step 2 description
+            "images": "", // Step 2 image
+        },
+    }
+}
