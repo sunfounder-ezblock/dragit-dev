645535e1948d89cbbe8432c9bca34a59c05b67f2
Cavon <cavonxx@gmail.com>
2019-02-28 10:18:50 +0800

    update

--- packages/raspberrypi/tutorials/block.js
+++ packages/raspberrypi/tutorials/block.js
@@ -36,109 +36,116 @@ RaspberryPi.tutorials.blockcode['Getting Start']['Blink LED'] = {
 
 RaspberryPi.tutorials.blockcode['Getting Start']['Breath LED'] = {
     "name": 'Breath LED',
-    "step1": {
-        "description": "step1",
-        "image": "",
-    },
-    "step2": {
-        "description": "step2",
-        "image": "",
-    },
-    "step3": {
-        "description": "step3",
-        "image": "",
-    },
-    "step4": {
-        "description": "step4",
-        "image": "",
-    },
-    "step5": {
-        "description": "step5",
-        "image": "",
-    },
-    "step6": {
-        "description": "step6",
-        "image": "",
-    },
+    "stepList": {
+        "step1": {
+            "description": "step1",
+            "image": "",
+        },
+        "step2": {
+            "description": "step2",
+            "image": "",
+        },
+        "step3": {
+            "description": "step3",
+            "image": "",
+        },
+        "step4": {
+            "description": "step4",
+            "image": "",
+        },
+        "step5": {
+            "description": "step5",
+            "image": "",
+        },
+        "step6": {
+            "description": "step6",
+            "image": "",
+        },
+    }
 }
 
 RaspberryPi.tutorials.blockcode['Advance']['Remote'] = {
     "name": 'Remote',
-    "step1": {
-        "description": "step1",
-        "image": "",
-    },
-    "step2": {
-        "description": "step2",
-        "image": "",
-    },
-    "step3": {
-        "description": "step3",
-        "image": "",
-    },
-    "step4": {
-        "description": "step4",
-        "image": "",
-    },
-    "step5": {
-        "description": "step5",
-        "image": "",
-    },
-    "step6": {
-        "description": "step6",
-        "image": "",
-    },
+    "stepList": {
+        "step1": {
+            "description": "step1",
+            "image": "",
+        },
+        "step2": {
+            "description": "step2",
+            "image": "",
+        },
+        "step3": {
+            "description": "step3",
+            "image": "",
+        },
+        "step4": {
+            "description": "step4",
+            "image": "",
+        },
+        "step5": {
+            "description": "step5",
+            "image": "",
+        },
+        "step6": {
+            "description": "step6",
+            "image": "",
+        },
+    }
 }
 
 RaspberryPi.tutorials.blockcode['Advance']['Wiring'] = {
     "name": 'Wiring',
-    "step1": {
-        "description": "step1",
-        "image": "tutorial-basic-hello-world-print-string.png",
-    },
-    "step2": {
-        "description": "step2",
-        "image": "tutorial-basic-hello-world-debug-monitor.png",
-    },
-    "step3": {
-        "description": "step3",
-        "image": "tutorial-basic-hello-world-print-string.png",
-    },
-    "step4": {
-        "description": "step4",
-        "image": "tutorial-basic-hello-world-debug-monitor.png",
-    },
-    "step5": {
-        "description": "step5",
-        "image": "tutorial-basic-hello-world-print-string.png",
-    },
-    "step6": {
-        "description": "step6",
-        "image": "tutorial-basic-hello-world-debug-monitor.png",
-    },
+    "stepList": {
+        "step1": {
+            "description": "step1",
+            "image": "tutorial-basic-hello-world-print-string.png",
+        },
+        "step2": {
+            "description": "step2",
+            "image": "tutorial-basic-hello-world-debug-monitor.png",
+        },
+        "step3": {
+            "description": "step3",
+            "image": "tutorial-basic-hello-world-print-string.png",
+        },
+        "step4": {
+            "description": "step4",
+            "image": "tutorial-basic-hello-world-debug-monitor.png",
+        },
+        "step5": {
+            "description": "step5",
+            "image": "tutorial-basic-hello-world-print-string.png",
+        },
+        "step6": {
+            "description": "step6",
+            "image": "tutorial-basic-hello-world-debug-monitor.png",
+        },
+    }
 }
 
 RaspberryPi.tutorials.blockcode['Basic']['Hello World'] = {
     "name": 'Hello World', // Name: Blink LED
-    // "steps": { // All Steps
-    "step1": { // Step 1
-        "description": "First project using the Raspberry Pi and the EZblock App. This project is used to learn more about Debug Monitor, it is a very useful tool to finish your projects.", // Step 1 description
-        "images": "", // Step 1 image
-    },
-    "step2": { // Step 2
-        "description": "After we create a new project, we can see a debug monitor in the left functional area. The debug monitor is used to print string, values or debug your code.", // Step 2 description
-        "images": "tutorial-basic-hello-world-debug-monitor.png", // Step 2 image
-    },
-    "step3": { // Step 2
-        "description": "Drag out 2 print block from the Basic category and drop to forever block, then input “What is your dream” and “My dream is world peace”. After flash the code to the Raspberry Pi, you can see the 2 string appear on the debug monitor.", // Step 2 description
-        "images": "tutorial-basic-hello-world-print-string.png", // Step 2 image
-    },
-    "step4": { // Step 2
-        "description": "It is a great tool if you want to read the value of one pins in your Raspberry Pi. Place a print block from the Basic category into forever block. Add a get pin A0 value block after the print block to print the A0 value. After flash the code to the Raspberry Pi, you can see the analog value of A0.", // Step 2 description
-        "images": "tutorial-basic-hello-world-print-value.png", // Step 2 image
-    },
-    "step5": { // Step 2
-        "description": "DO NOT ignore any errors appeared on the debug monitor, if you can’t solve the error, then the code will never download successfully.", // Step 2 description
-        "images": "", // Step 2 image
-    },
+    "stepList": { // All Steps
+        "step1": { // Step 1
+            "description": "First project using the Raspberry Pi and the EZblock App. This project is used to learn more about Debug Monitor, it is a very useful tool to finish your projects.", // Step 1 description
+            "images": "", // Step 1 image
+        },
+        "step2": { // Step 2
+            "description": "After we create a new project, we can see a debug monitor in the left functional area. The debug monitor is used to print string, values or debug your code.", // Step 2 description
+            "images": "tutorial-basic-hello-world-debug-monitor", // Step 2 image
+        },
+        "step3": { // Step 2
+            "description": "Drag out 2 print block from the Basic category and drop to forever block, then input “What is your dream” and “My dream is world peace”. After flash the code to the Raspberry Pi, you can see the 2 string appear on the debug monitor.", // Step 2 description
+            "images": "tutorial-basic-hello-world-print-string", // Step 2 image
+        },
+        "step4": { // Step 2
+            "description": "It is a great tool if you want to read the value of one pins in your Raspberry Pi. Place a print block from the Basic category into forever block. Add a get pin A0 value block after the print block to print the A0 value. After flash the code to the Raspberry Pi, you can see the analog value of A0.", // Step 2 description
+            "images": "tutorial-basic-hello-world-print-value", // Step 2 image
+        },
+        "step5": { // Step 2
+            "description": "DO NOT ignore any errors appeared on the debug monitor, if you can’t solve the error, then the code will never download successfully.", // Step 2 description
+            "images": "", // Step 2 image
+        },
+    }
 }
